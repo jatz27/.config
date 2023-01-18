ed130ce791e840ac1202cc86a9eb90c0ec4afcb5
Julio Atz <julioatz90@gmail.com>
2023-01-17 18:40:02 -0600

    Change character starship

--- nvim/lua/user/whichkey.lua
+++ nvim/lua/user/whichkey.lua
@@ -184,7 +184,7 @@ local mappings = {
     t = { "<cmd>lua _PWSH_TOGGLE()<cr>", "PowerShell" },
     p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
     --[[ f = { "<cmd>ToggleTerm direction=float<cr>", "Float" }, ]]
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
+    h = { "<cmd>ToggleTerm size=12 direction=horizontal<cr>", "Horizontal" },
     v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
   },
 
